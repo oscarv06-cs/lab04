ecdc744d8c4ec7023198f39f4522835582341eca
Oscar Ramon Valeriano Leiva <oscarvaleriano@csilvm-03.cs.ucsb.edu>
2025-11-03 22:53:37 -0800

    done with customeitem.h

--- CustomItem.h
+++ CustomItem.h
@@ -1,14 +1,16 @@
 #ifndef CUSTOMITEM_H
-#define CUSTOMEITEM_H
+#define CUSTOMITEM_H
 #include <map>
 #include <string>
 #include "IceCreamItem.h"
-class CustomeItem:public IceCreamItem{
+class CustomItem:public IceCreamItem{
     std::map<std::string, int> toppings;
-
 public:
-    CustomItem(std::string size);
+    explicit CustomItem(std::string size);
+    virtual ~CustomItem() = default;
+    std::string composeItem() const override;
+    void addTopping(const std::string& topping);
     
-}
+};
 
 #endif
